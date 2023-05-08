45ff545a1c624d9fcc415e4b155c88f47b522d08
Luca Bailey <mail@photon.garden>
2023-05-08 00:09:49 -0400

    Refactor artwork/root.rs.

--- src/artwork/root.rs
+++ src/artwork/root.rs
@@ -5,15 +5,10 @@ pub fn create(_params: CreateArtworkParams) -> RegularPolygons {
     // let radius = sine().frequency(1.0).denormalize(0.003, 0.019);
     // .crystallize();
 
-    let radius = smart_sine()
-        .frequency(1.0)
-        .denormalize_generator(0.003, 0.019)
-        .map_input(|xy: Point2| xy.y);
-
     regular_polygons()
         .num_repeats(100)
-        // .resolution(random_usize(5, 25))
-        .resolution(25)
+        .resolution(random_usize(5, 25))
+        // .resolution(25)
         .stroke_weight(0.001)
         .color(
             repeated_colors()
@@ -21,8 +16,7 @@ pub fn create(_params: CreateArtworkParams) -> RegularPolygons {
                 .color_picker(254, 162, 151, 255)
                 .color_picker(164, 222, 251, 255),
         )
-        .smart_radius(radius)
-        // .smart_radius(|_: &Rand, xy: Point2| xy.y.denormalize(0.003, 0.019))
+        .smart_radius(radius())
         .center(center())
         .polygon_is_filled(flip_coin(0.5))
 }
@@ -33,6 +27,14 @@ fn center() -> impl IntoPointGenerator {
         .y_resolution(10)
         .x_jitter(0.001)
         .y_jitter(0.000)
+}
+
+fn radius() -> impl IntoSmartGenerator<Point2, f32> {
+    // smart_sine()
+    //     .frequency(1.0)
+    //     .denormalize_generator(0.003, 0.019)
+    //     .map_input(|xy: Point2| xy.y)
+    |_: &Rand, xy: Point2| xy.y.denormalize(0.003, 0.019)
 }
 
 pub struct CreateArtworkParams<'a> {
